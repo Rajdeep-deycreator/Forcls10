bc05c3452c3bd146b03174eb98246e5e0da3b6fd
Rajdeep-deycreator <deyrajdeep569@gmail.com>
2026-02-16 20:53:06 +0530

    update

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGuhMm8B612JZlVuNQ7C6cV4YahM7JhKw",
+  authDomain: "bestwishes-dad65.firebaseapp.com",
+  projectId: "bestwishes-dad65",
+  storageBucket: "bestwishes-dad65.firebasestorage.app",
+  messagingSenderId: "985234171383",
+  appId: "1:985234171383:web:290bcfaaf5bd19dfce3af2",
+  measurementId: "G-F6XJKD7JXL"
+};
+firebase.initializeApp(firebaseConfig);
+const analytics=firebase.analytics();
 setTimeout(()=>{
   document.body.style.animation= "rise 5s ease-in-out forwards"
 },4000)
